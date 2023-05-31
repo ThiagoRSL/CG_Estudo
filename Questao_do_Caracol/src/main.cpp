419de910e1ab6fd63d36feecf889f8f2a228784e
Thiago R Silva <47826823+ThiagoRSL@users.noreply.github.com>
2023-05-31 09:27:09 -0300

    Criada questão do Caracol

--- Questao_do_Caracol/src/main.cpp
+++ Questao_do_Caracol/src/main.cpp
@@ -31,32 +31,61 @@
 #include "Mat.h"
 #include "CameraManager.h"
 
+//Inicio dos Eixos
+float initial_x;
+float initial_y;
+//
 
 // QUESTÃO DO QUADRADO (OPERAÇÕES EM VETORES, MATRIZES ETC) //
-Mat *M, *M2, *M3, *M4, *M5;
-int initial_x, initial_y;
-float linex, liney;
+float angle;
+float radius;
+float angle_variation;
+float radius_variation;
 // -------------------------------------------------------- //
 
-//Desenha o Quadrado
+//Desenha o Caracol
+/*
+    Esta questão utiliza conhecimento de coordenadas polares, para isto os pontos são mapeados utilizando raio e angulo ao invés de x e y.
+    o x e o y são calculados na hora de desenhar com base nas coordenadas originais.
+*/
 void render()
 {
     CV::clear(0,0,0);
-
     CV::color(1,1,1);
     // Desenha Eixos X e Y
     CV::line(initial_x,initial_y,initial_x+1600,initial_y);
     CV::line(initial_x,initial_y,initial_x,0);
 
-    // Desenha linha formada por (x1, y0) e (x0, y1);
+    //Questão
     CV::color(7);
-    CV::line(initial_x,initial_y-liney,initial_x+linex,initial_y);
 
-    M->Draw(initial_x, initial_y, 2);
-    M2->Draw(initial_x, initial_y, 3);
-    M3->Draw(initial_x, initial_y, 4);
-    M4->Draw(initial_x, initial_y, 5);
-    M5->Draw(initial_x, initial_y, 6);
+    angle = 0.0;
+    radius = 1.1;
+    angle_variation = -0.1;
+    radius_variation = 1.0;
+
+    float x, y, x_temp, y_temp;
+    int i;
+    for (i = 0; i < 1000; i++)
+    {
+        //Cria o vetor do raio paralelo ao eixo X
+        x = radius;
+        y = 0;
+
+        //Aplica a rotação neste vetor no angulo alfa
+        x_temp = x*cos(angle) + y*(-sin(angle));
+        y_temp = x*sin(angle) + y*(cos(angle));
+
+        //  Note que, coomo o y é igual a zero, a aplicação da matrix de rotação poderia ser simplificada para:
+
+        //  x_temp = x*cos(angle) + y*(-sin(angle));    =>   x_temp = x*cos(angle));   =>  x_temp = radius*cos(angle));
+        //  y_temp = x*sin(angle) + y*(cos(angle));     =>   y_temp = x*sin(angle));   =>  y_temp = radius*sin(angle));
+
+        CV::circle(x_temp+initial_x, initial_y-y_temp, 2, 10);
+        radius += radius_variation;
+        angle += angle_variation;
+    }
+
 }
 
 void keyboard(int key)
@@ -78,112 +107,21 @@ void mouse(int button, int state, int wheel, int direction, int x, int y)
 
 int main(void)
 {
+    //Variáveis para visualização
     initial_x = 500;
     initial_y = 670;
-
-    float x1, y1, L;
-    float x2 = 400, y2 = 400;
-    linex = x2;
-    liney = y2;
-
-    L = 100;
-    x1 = -L;
-    y1 = 1;
-
-    /* Define a matriz com os vetores que formam os pontos do quadrado. */
-    M = new Mat(3,4);
-    *M->At(0,0) = x1;
-    *M->At(1,0) = y1;
-    *M->At(2,0) = 1;
-    *M->At(0,1) = x1 + L;
-    *M->At(1,1) = y1;
-    *M->At(2,1) = 1;
-    *M->At(0,2) = x1 + L;
-    *M->At(1,2) = y1 - L;
-    *M->At(2,2) = 1;
-    *M->At(0,3) = x1;
-    *M->At(1,3) = y1 - L;
-    *M->At(2,3) = 1;
-    /* ------------------- */
-
-    /* Define a matriz de translação que tras o quadrado à Origem. */
-    Mat T1 = Mat(3,3);
-    *T1.At(0,0) = 1;
-    *T1.At(0,2) = L;
-    *T1.At(1,1) = 1;
-    *T1.At(1,2) = L;
-    *T1.At(2,2) = 1;
-    /* ------------------- */
-
-    /* Define a matriz de para adequar a escala, alterando o tamanho dos lados para 1/3 do tamanho original. */
-    Mat E = Mat(3,3);
-    *E.At(0,0) = 1/3.0;
-    *E.At(1,1) = 1/3.0;
-    *E.At(2,2) = 1;
-    /* ------------------- */
-
     //
-    //O Vetor formado por (-x1, y1) é paralelo ao vetor formado por (x1, 0) e (0, y1)
-    //Define o v1 como o vetor formado pelos pontos x1 e y1 da questão.
-    //
-    Vec2 v1 = Vec2(-x2, y2);
-    //Normaliza o vetor para cortar a divisão do produto interno futuramente
-    v1.Normalize();
-    //Define um vetor já normalizado paralelo ao eixo Y.
-    Vec2 v2 = Vec2(0, 1);
-    //Encontra o angulo atraves do produto interno dos dois vetores.
-    float alfa = acos(v1.DotProduct(&v2));
 
-    /* Define a matriz de rotação. */
-    Mat R = Mat(3,3);
-    *R.At(0,0) = cos(alfa);
-    *R.At(0,1) = -sin(alfa);
-    *R.At(1,0) = sin(alfa);
-    *R.At(1,1) = cos(alfa);
-    *R.At(2,2) = 1;
-    /* ------------------- */
 
-    /* Define a matriz de translação para deslocar o quadrado para o ponto alvo. */
-    Mat T2 = Mat(3,3);
-    *T2.At(0,0) = 1;
-    *T2.At(0,2) = x2;
-    *T2.At(1,1) = 1;
-    *T2.At(1,2) = 0;
-    *T2.At(2,2) = 1;
-    /* ------------------- */
+    CV::init("Questão do Caracol.");
 
-    /* Salva as operações */
-    M2 = Mat::Mult(&T1, M);     // Aplica a primeira translação para trazer o quadrado para origem.
-    M3 = Mat::Mult(&E, M2);     // Aplica a escala definida, 1/3 do tamanho original.
-    M4 = Mat::Mult(&R, M3);     // Aplica a rotação.
-    M5 = Mat::Mult(&T2, M4);    // Aplica a translação para deslocar ao ponto desejado.
-    /* ------------------- */
-
-    /* PRINTS DAS MATRIZES */
-    printf("\nMatrix M");
-    M->ShowMatrix();
-    printf("\nMatrix T1");
-    T1.ShowMatrix();
-    printf("\nMatrix E");
-    E.ShowMatrix();
-    printf("\nMatrix R");
-    R.ShowMatrix();
-    printf("\nMatrix T2");
-    T2.ShowMatrix();
-    printf("\nMatrix M2");
-    M2->ShowMatrix();
-    printf("\nMatrix M3");
-    M3->ShowMatrix();
-    printf("\nMatrix M4");
-    M4->ShowMatrix();
-    printf("\nMatrix M5");
-    M5->ShowMatrix();
-    /* ------------------- */
-
-    CV::init("Questão do Quadrado.");
 
     int screenWidth = glutGet(GLUT_SCREEN_WIDTH);
     int screenHeight = glutGet(GLUT_SCREEN_HEIGHT);
+
+    //Definindo ponto origem no centro da tela.
+    initial_x = screenWidth/2;
+    initial_y = screenHeight/2;
 
     glutSetCursor(GLUT_CURSOR_CROSSHAIR);
     CV::run();
